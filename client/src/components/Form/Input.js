1fcf6f3b0b63f05c5f83d8fe55c40c662913ad45
rickiangela <rickiangela@gmail.com>
2019-04-16 20:07:13 -0400

    code

--- client/src/components/Form/Input.js
+++ client/src/components/Form/Input.js
@@ -7,7 +7,3 @@ export const Input = props => (
     <input className="form-control" {...props}/>
   </div>
 );
-
-// make sure the <Input> component uses a closing tag rather than being self-closing so you can put the label text in the {props.children} location
-
-//  You will also need props for the 'for' and 'id' properties (labeled above as 'props.control'), as well as the placeholder.
